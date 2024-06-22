bf5c8c778279672550f3f804c4f82cd199087dce
dandev947366 <dan.dev947366@gmail.com>
2024-06-22 10:06:17 +0300

    add Icons

--- src/components/layout/MenuItem.tsx
+++ src/components/layout/MenuItem.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const MenuItem = ({ url = "/", title = "" }: { url: string; title: string }) => {
+const MenuItem = ({ url = "/", title = "", icon }: { url: string; title: string; icon?:React.ReactNode }) => {
   return (
     <li className="mb-2">
-      <a href={url} className="text-white p-3 rounded-md flex items-center">
+      <a href={url} className="text-white p-3 rounded-md flex items-center gap-2">
+      {icon}
         {title}
       </a>
     </li>
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import MenuItem from "./MenuItem";
-
+import IconPlay from "../icons/IconPlay"
+import IconExplore from "../icons/IconExplore";
 const Sidebar = () => {
   return (
     <div className="p-5">
       <h2 className="font-bold text-2xl p-2 text-blue-200"><a href="/">Elearn Platform</a></h2>
+      
       <ul>
-        <MenuItem url="/learn" title="Learning Zone" />
-        <MenuItem url="/explore-courses" title="Explore Courses" />
+        <MenuItem url="/learn" title="Learning Zone" icon={<IconPlay className="size-5"  />}/>
+        <MenuItem url="/explore-courses" title="Explore Courses" icon={<IconExplore className="size-5"  />}/>
         <MenuItem url="/contact" title="Contact Us" />
         <MenuItem url="/about" title="About Us" />
         
