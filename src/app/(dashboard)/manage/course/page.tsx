93fe62fd74e43202e0edec5e25aa8e504adc7a7f
dandev947366 <dan.dev947366@gmail.com>
2024-06-23 11:44:47 +0300

    update pages

--- src/app/(dashboard)/manage/course/page.tsx
+++ src/app/(dashboard)/manage/course/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Heading from "@/components/typography/Heading";
+import CourseItem from "@/components/course/CourseItem";
+import { CourseGrid } from "@/components/common";
 const page = () => {
   return (
-    <div>course page</div>
+  <div>
+    <Heading><h1 className="text-3xl font-bold"> My Courses</h1></Heading>
+    <CourseGrid>
+    <CourseItem />
+    <CourseItem />
+    <CourseItem />
+    </CourseGrid>  
+    </div>
   )
 }
 
--- src/app/(dashboard)/study/page.tsx
+++ src/app/(dashboard)/study/page.tsx
@@ -4,11 +4,15 @@ import CourseItem from "@/components/course/CourseItem";
 import { CourseGrid } from "@/components/common";
 const page = () => {
   return (
-    <div>
-    <Heading><h1 className="text-3xl font-bold"> Learning Courses</h1></Heading>
-  
+  <div>
+    <Heading><h1 className="text-3xl font-bold"> Study Zone</h1></Heading>
+    <CourseGrid>
+    <CourseItem />
+    <CourseItem />
+    <CourseItem />
+    </CourseGrid>  
     </div>
-    )
+  )
 }
 
 export default page
